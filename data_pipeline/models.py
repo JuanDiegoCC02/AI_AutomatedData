37b7daee2f4fc69ef4aa27c6362822fa16fe2a1f
Juan Diego C.C. <dcorella2324@gmail.com>
2026-06-12 14:49:20 -0600

    feat: create modelDocument and cleaner structure

--- data_pipeline/models.py
+++ data_pipeline/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+class Document(models.Model):
+
+    filename = models.CharField(max_length=255)
+
+    total_characters = models.IntegerField()
+
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.filename
